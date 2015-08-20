72fccbdbf048c5ed550f4407eea58f9eda69b4c7
Generic Person <gene@crithead.com>
2015-08-20 09:44:33 -0700

    Removed ''registry verion'' of test suite selection.

--- testmain.cpp
+++ testmain.cpp
@@ -1,54 +1,5 @@
 // testmain.cpp
-#define USE_VERISON_ONE 0
-#define USE_VERISON_TWO 1
 
-#if 0 // USE_VERSION_ONE
-//
-// http://c2.com/cgi/wiki?DirtSimpleCppUnitExample
-//
-
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
-#include <cppunit/CompilerOutputter.h>
-
-CppUnit::Test *suite( const char *suiteName )
-{
-	CppUnit::TestFactoryRegistry &registry =
-		CppUnit::TestFactoryRegistry::getRegistry();
-
-	registry.registerFactory(
-		&CppUnit::TestFactoryRegistry::getRegistry( suiteName ) );
-	return registry.makeTest();
-}
-
-
-int main( int argc, char* argv[] )
-{
-	// if command line contains "-selftest" then this is the post build check
-	// => the output must be in the compiler error format.
-	bool selfTest = (argc > 1)  &&
-		(std::string("-selftest") == argv[1]);
-
-	CppUnit::TextUi::TestRunner runner;
-	// Add the top suite to the test runner
-	runner.addTest( suite( "ItemTest" ) );
-	runner.addTest( suite( "PlayerTest" ) );
-
-	if ( selfTest ) {
-		// Change the default outputter to a compiler error format outputter
-		// The test runner owns the new outputter.
-		runner.setOutputter( CppUnit::CompilerOutputter::defaultOutputter(
-			&runner.result(), std::cerr ) );
-	}
-
-	// Run the test.
-	bool wasSucessful = runner.run( "" );
-
-	// Return error code 1 if any tests failed.
-	return wasSucessful ? 0 : 1;
-}
-
-#else // USE_VERSION_TWO
 //
 // http://cppunit.sourceforge.net/doc/1.8.0/cppunit_cookbook.html
 //
